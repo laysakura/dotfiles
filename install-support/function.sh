5b311fadcdec89541d89b8d98d46503aa88ad9f0
Sho Nakatani <lay.sakura@gmail.com>
2016-08-04 09:01:21 -0700

    書いてる

--- install-support/function.sh
+++ install-support/function.sh
@@ -14,3 +14,18 @@ function is_linux() { [[ `ostype` == linux* ]]; }
 function is_osx() { [[ `ostype` == darwin* ]]; }
 function is_bsd() { [[ `ostype` == bsd* ]]; }
 function is_msys() { [[ `ostype` == msys* ]]; }
+
+# git操作
+## git管理されているファイルを丸っとコピー
+function gitCopy() {
+    local srcRepoDir=$1
+    local destDir=$2
+    local treeIsh=$3
+    local tarFile=`mktemp`.tar
+    (
+        cd $srcRepoDir
+        git archive -o $tarFile $treeIsh
+        cd $destDir
+        tar xf $tarFile
+    )
+}
--- install.bash
+++ install.bash
@@ -31,30 +31,31 @@ function repoUrl() {
 # インストール実行
 function run() {
     local tmpdir=`mktemp -d`
-
-    # ローカルに存在していなかったら、privateリポジトリから dotfiles-secret を一時的にclone
-    # TODO ローカルにあったらそれを使う
-    # git clone $(repoUrl ${REPO_DOTFILES_SECRET[site]} ${REPO_DOTFILES_SECRET[name]}) "$tmpdir/${REPO_DOTFILES_SECRET[name]}"
+    local dotfilesDir=$tmpdir/${REPO_DOTFILES[name]}
+    local dotfilesSecretDir=$tmpdir/${REPO_DOTFILES_SECRET[name]}
 
     # ローカルに存在していなかったら、publicリポジトリから dotfilesを 一時的にclone
     # TODO ローカルにあったらそれを使う
-    git clone $(repoUrl ${REPO_DOTFILES[site]} ${REPO_DOTFILES[name]}) "$tmpdir/${REPO_DOTFILES[name]}"
+    git clone $(repoUrl ${REPO_DOTFILES[site]} ${REPO_DOTFILES[name]}) $dotfilesDir
 
-    # 補助スクリプトが手に入ったのでsource
-    local support_dir="$tmpdir/${REPO_DOTFILES[name]}/install-support"
-    . "$support_dir/function.sh"
+    # ローカルに存在していなかったら、privateリポジトリから dotfiles-secret を一時的にclone
+    # TODO ローカルにあったらそれを使う
+    git clone $(repoUrl ${REPO_DOTFILES_SECRET[site]} ${REPO_DOTFILES_SECRET[name]}) $dotfilesSecretDir
 
-    ostype
+    # 補助スクリプトが手に入ったのでsourceしていく
+    local supportDir=$tmpdir/${REPO_DOTFILES[name]}/install-support
+    ## 便利関数定義
+    . $supportDir/function.sh
 
-    # 各ソフトの設定をばらまく
+    # dotfiles-secretをdotfilesにマージ
+    gitCopy $dotfilesSecretDir $dotfilesDir
 
-    # 最低限のシェル関数を定義し、zshやほかのソフトをインストール
+    # 設定ファイルの配置
+    . $supportDir/installDotfiles.sh
+
+    # 利用するソフトウェアのインストール
+    . $supportDir/installPackages.sh
+
+    # ログインシェルの変更
 }
 run
-
-
-
-
-# ログインシェルをzshに変更する
-# => zshrcが読み込まれるが、その最初の最初で必要ソフトのインストールを走らせる
-# => その次くらいに、現行のinstall.shでやっているようなほかのソフトの設定をやってしまう
