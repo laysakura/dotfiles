d03c136da6cb133016e9abb85ecc3157e9cf0ffd
Sho Nakatani <lay.sakura@gmail.com>
2016-08-04 07:29:06 -0700

    cloneできてる

---
+++ install.bash
@@ -0,0 +1,51 @@
+#!/bin/bash -xeu
+
+# TODO install-min/ とかに入れてsourceする
+declare -A SITE_URL=(
+    [bitbucket]='bitbucket.org'
+    [github]='github.com'
+)
+
+declare -A SITE_USER=(
+    [bitbucket]='laysakura'
+    [github]='laysakura'
+)
+
+declare -A REPO_DOTFILES_SECRET=(
+    [site]='bitbucket'
+    [name]='laysakura/dotfiles-secret'
+)
+
+declare -A REPO_DOTFILES=(
+    [site]='github'
+    [name]='laysakura/dotfiles'
+)
+
+
+function repoUrl() {
+    local repoSite=$1
+    local repoName=$2
+    echo -n "https://${SITE_USER[$repoSite]}@${SITE_URL[$repoSite]}/$repoName.git"
+}
+
+function run() {
+    local tmpdir=`mktemp -d`
+
+    # ローカルに存在していなかったら、privateリポジトリから dotfiles-secret を一時的にclone
+    # TODO ローカルにあったらそれを使う
+    git clone $(repoUrl ${REPO_DOTFILES_SECRET[site]} ${REPO_DOTFILES_SECRET[name]}) "$tmpdir/${REPO_DOTFILES_SECRET[name]}"
+
+    # ローカルに存在していなかったら、publicリポジトリから dotfilesを 一時的にclone
+    # TODO ローカルにあったらそれを使う
+    git clone $(repoUrl ${REPO_DOTFILES[site]} ${REPO_DOTFILES[name]}) "$tmpdir/${REPO_DOTFILES[name]}"
+}
+run
+
+
+# 最低限のシェル関数を定義し、zshやほかのソフトをインストール
+
+# 各ソフトの設定をばらまく
+
+# ログインシェルをzshに変更する
+# => zshrcが読み込まれるが、その最初の最初で必要ソフトのインストールを走らせる
+# => その次くらいに、現行のinstall.shでやっているようなほかのソフトの設定をやってしまう
