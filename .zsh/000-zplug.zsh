b857d2939aec12c07befb2428d4ebe3392db3d03
Sho Nakatani <lay.sakura@gmail.com>
2016-08-22 15:45:23 +0900

    zplug install

--- .zsh/000-zplug.zsh
+++ .zsh/000-zplug.zsh
@@ -4,4 +4,5 @@ zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 
 # Then, source plugins and add commands to $PATH
+zplug check || zplug install
 zplug load --verbose
