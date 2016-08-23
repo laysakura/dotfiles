1e4d8496aa5e192366342859ea84662f9612df1d
Sho Nakatani <lay.sakura@gmail.com>
2016-08-24 08:04:19 +0900

    dbusに願いを

--- install.bash
+++ install.bash
@@ -48,6 +48,37 @@ function die() {
     exit ${2:-1}
 }
 
+# Unity上でのdbusの問題を解決するおまじない -> http://askubuntu.com/questions/457016/how-to-change-gsettings-via-remote-shell
+function dbusUnityMagic() {
+    # Search these processes for the session variable 
+    # (they are run as the current user and have the DBUS session variable set)
+    compatiblePrograms=( nautilus kdeinit kded4 pulseaudio trackerd )
+
+    # Attempt to get a program pid
+    for index in ${compatiblePrograms[@]}; do
+        PID=$(pidof -s ${index})
+        if [[ "${PID}" != "" ]]; then
+            break
+        fi
+    done
+    if [[ "${PID}" == "" ]]; then
+        echo "Could not detect active login session"
+        return 1
+    fi
+
+    QUERY_ENVIRON="$(tr '\0' '\n' < /proc/${PID}/environ | grep "DBUS_SESSION_BUS_ADDRESS" | cut -d "=" -f 2-)"
+    if [[ "${QUERY_ENVIRON}" != "" ]]; then
+        export DBUS_SESSION_BUS_ADDRESS="${QUERY_ENVIRON}"
+        echo "Connected to session:"
+        echo "DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS}"
+    else
+        echo "Could not find dbus session ID in user environment."
+        return 1
+    fi
+
+    return 0
+}
+
 # インストール実行
 function run() {
     local tmpdir=`mktemp -d`
@@ -82,6 +113,7 @@ function run() {
     logOk "source-ed $supportDir/function.sh"
 
     # 一回だけ実行すれば良いもの(冪等性は必要)
+    dbusUnityMagic
     is_ubuntu && gsettings set org.gnome.settings-daemon.plugins.keyboard active false  # fcitx切り替えの際にxkbキーバインドが戻らないようにする
     is_ubuntu && gsettings set org.gnome.desktop.interface gtk-key-theme Emacs          # 操作を全体的にEmacs風に
 
