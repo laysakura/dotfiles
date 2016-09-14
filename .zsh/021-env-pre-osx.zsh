0648399e6a24b216bd35331fa78308f731a157f1
Sho Nakatani <lay.sakura@gmail.com>
2016-09-14 17:01:52 +0900

    Update 021-env-pre-osx.zsh

--- .zsh/021-env-pre-osx.zsh
+++ .zsh/021-env-pre-osx.zsh
@@ -3,5 +3,5 @@ is_osx || return 0
 # Homebrew
 export BREWPATH=$HOME/.homebrew
 export PATH=$BREWPATH/bin:$PATH
-export MANPATH=$BREWPATH/share/man:$MANPATH
-export INFOPATH=$BREWPATH/share/info:$INFOPATH
+export MANPATH=$BREWPATH/share/man:${MANPATH:-}
+export INFOPATH=$BREWPATH/share/info:${INFOPATH:-}
