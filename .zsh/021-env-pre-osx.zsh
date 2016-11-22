44eb4b6d21870ae85db9dae2728c9ae67a5fb781
Sho Nakatani <lay.sakura@gmail.com>
2016-11-22 16:39:55 +0900

    homebrewはsbinに実行ファイルインストールされることもあるんやなぁ

--- .zsh/021-env-pre-osx.zsh
+++ .zsh/021-env-pre-osx.zsh
@@ -3,6 +3,7 @@ is_osx || return 0
 # Homebrew
 export BREWPATH=$HOME/.homebrew
 export PATH=$BREWPATH/bin:$PATH
+export PATH=$BREWPATH/sbin:$PATH
 export MANPATH=$BREWPATH/share/man:${MANPATH:-}
 export INFOPATH=$BREWPATH/share/info:${INFOPATH:-}
 
