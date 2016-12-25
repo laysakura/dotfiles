b60b49542bc9160e16617e3a59aa92c1d74eaf41
Sho Nakatani <lay.sakura@gmail.com>
2016-12-25 15:14:39 +0900

    x

--- emacs/.emacs.d/elisp/my-flycheck.el
+++ emacs/.emacs.d/elisp/my-flycheck.el
@@ -1,5 +1,4 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(eval-after-load 'flycheck
-  '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+(with-eval-after-load 'flycheck
+  (flycheck-pos-tip-mode))
