ffb341ae83433cc81a92b02964d8d912a2b229f2
Sho Nakatani <lay.sakura@gmail.com>
2016-12-25 14:52:08 +0900

    x

--- emacs/.emacs.d/elisp/my-company-mode.el
+++ emacs/.emacs.d/elisp/my-company-mode.el
@@ -4,3 +4,12 @@
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 1) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+;; C-n, C-pで補完候補を次/前の候補を選択
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+
+;; TABで候補を設定
+(define-key company-active-map (kbd "C-i") 'company-complete-selection)
