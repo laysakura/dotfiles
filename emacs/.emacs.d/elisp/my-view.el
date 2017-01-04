f2918e21ec46523c653657e2bfef86df562d2b37
Sho Nakatani <lay.sakura@gmail.com>
2017-01-05 07:05:06 +0900

    emacsの色で見づらいものがあったのでカスタマイズ

--- emacs/.emacs.d/elisp/my-view.el
+++ emacs/.emacs.d/elisp/my-view.el
@@ -1,3 +1,10 @@
 (load-theme 'monokai t)
 
 (menu-bar-mode -1)
+
+(custom-set-faces
+ '(font-lock-comment-face ((t (:foreground "color-167"))))
+
+ '(flycheck-warning ((t (:background "color-58") (:foreground "color-255"))))
+ '(flycheck-fringe-warning ((t (:background "color-58") (:foreground "color-255"))))
+)
