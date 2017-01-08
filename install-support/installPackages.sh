1950dbee98e69da7e897f2b45997e77d5f2392c1
Sho Nakatani <lay.sakura@gmail.com>
2017-01-08 10:16:55 +0900

    JDK入れられない環境のセットアップ中なので、一旦flywayを外す

--- install-support/installPackages.sh
+++ install-support/installPackages.sh
@@ -61,7 +61,7 @@ function runInstallPackages() {
     installPackage awscli
     installPackage bash
     installPackage emacs
-    installPackage flyway
+    #installPackage flyway
     installPackage git
     installPackage gnu-sed
     installPackage libevent
