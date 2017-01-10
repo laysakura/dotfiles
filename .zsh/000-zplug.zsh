1a0839bce606c7b888de7d646f138fbf89d96236
snakatani <snakatani@folio-sec.com>
2017-01-10 11:12:19 +0900

    ウザい警告を消す

--- .zsh/000-zplug.zsh
+++ .zsh/000-zplug.zsh
@@ -1,7 +1,7 @@
 source ~/.zplug/init.zsh
 
 zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "zsh-users/zsh-syntax-highlighting", defer:10
 
 # Then, source plugins and add commands to $PATH
 zplug check || zplug install
