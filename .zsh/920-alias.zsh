040787d73d34c0faa34d5bfd34448e781aa3e31a
snakatani <snakatani@folio-sec.com>
2017-02-27 14:25:51 +0900

    color

--- .zsh/920-alias.zsh
+++ .zsh/920-alias.zsh
@@ -2,3 +2,5 @@ alias emacs="emacs -nw"
 alias be="bundle exec"
 alias plantuml="java -jar $HOME/.plantuml.jar -charset UTF-8"
 alias q="rlwrap -c -r q"
+alias scala='scala -Dscala.color'
+alias sbt='sbt -Dscala.color'
