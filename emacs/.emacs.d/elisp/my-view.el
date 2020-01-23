760b84293bd215be5a550ac2d2eea94c3cbfc27b
Sho Nakatani <lay.sakura@gmail.com>
2020-01-23 22:03:51 +0900

    emacs color

--- emacs/.emacs.d/elisp/my-view.el
+++ emacs/.emacs.d/elisp/my-view.el
@@ -9,3 +9,15 @@
  '(flycheck-warning ((t (:background "color-58") (:foreground "color-255"))))
  '(flycheck-fringe-warning ((t (:background "color-58") (:foreground "color-255"))))
 )
+
+;; Color
+(progn
+  ;; モードラインの文字の色を設定します。
+  (set-face-foreground 'mode-line "#ffffff")
+  ;; モードラインの背景色を設定します。
+  (set-face-background 'mode-line "color-20")
+  ;; モードライン（アクティブでないバッファ）の文字色を設定します。
+  (set-face-foreground 'mode-line-inactive "white")
+  ;; モードライン（アクティブでないバッファ）の背景色を設定します。
+  (set-face-background 'mode-line-inactive "color-17")
+)
