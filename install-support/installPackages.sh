8c5cb626bfb94d03b174e9b43b8bc706bf81070f
Sho Nakatani <lay.sakura@gmail.com>
2020-05-12 15:54:54 +0900

    ubuntu

--- install-support/installPackages.sh
+++ install-support/installPackages.sh
@@ -84,37 +84,12 @@ function runInstallPackages() {
 
     has rustc || (curl https://sh.rustup.rs -sSf | sh)  # rustに付属するcargo
 
-    if ! has rbenv; then  # gem i
-        git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-        git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-        ruby_version=$(rbenv install -l | grep -v - | tail -1)
-        rbenv install $ruby_version
-        rbenv global $ruby_version
-    fi
-
     # root package managerでインストールする諸々
-    installPackage awscli
-    installPackage bash
-    installPackage direnv
     installPackage emacs
-    installPackage git
-    installPackage gnu-sed
-    installPackage graphicsmagick
-    installPackage graphviz
-    installPackage jq
-    installPackage lazydocker
-    installPackage libevent
-    installPackage libssh2
-    installPackage nkf
-    installPackage npm
-    installPackage openssl
     installPackage peco
     installPackage pkg-config
-    installPackage pyenv
     installPackage readline
     installPackage reattach-to-user-namespace
-    installPackage terminal-notifier
-    installPackage the_silver_searcher
     installPackage tmux
     installPackage tree
     installPackage vim
@@ -128,23 +103,17 @@ function runInstallPackages() {
         cask install
     )
 
-    # gemでインストール
-    gem i travis
-
     # その他のコマンドでインストール
     if ! has diff-highlight; then
         curl https://raw.githubusercontent.com/git/git/3dbfe2b8ae94cbdae5f3d32581aedaa5510fdc87/contrib/diff-highlight/diff-highlight -o $HOME/.bin/diff-highlight
         chmod +x $HOME/.bin/diff-highlight
     fi
 
-    has nodebrew || (curl -L git.io/nodebrew |perl - setup)
-
-    has whalebrew || (curl -L "https://github.com/bfirsh/whalebrew/releases/download/0.0.5/whalebrew-$(uname -s)-$(uname -m)" -o $HOME/.bin/whalebrew; chmod +x $HOME/.bin/whalebrew)
-
     # osx専用パッケージのインストール
     if is_osx; then
         installPackage reattach-to-user-namespace  # see: http://totutotu.hatenablog.com/entry/2015/07/26/tmux%E3%81%A7OSX%E3%81%AEopen%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%84
         installPackage gnu-sed
+        installPackage terminal-notifier
 
         brew tap homebrew/services
 
