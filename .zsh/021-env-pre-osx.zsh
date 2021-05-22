672dccc72e8c7760403e700de2517effd4552e9b
Sho Nakatani <lay.sakura@gmail.com>
2021-05-22 14:23:00 +0900

    ssh agent

--- .zsh/021-env-pre-osx.zsh
+++ .zsh/021-env-pre-osx.zsh
@@ -11,6 +11,9 @@ export HOMEBREW_CASK_OPTS="--require-sha --appdir=~/Applications"
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 export HOMEBREW_NO_ANALYTICS=1
 
+# ssh-agent
+ssh-add -K ~/.ssh/id_rsa_github_nopass
+
 # Python (pyenv is supposed to be installed via homebrew)
 eval "$(pyenv init -)"
 
