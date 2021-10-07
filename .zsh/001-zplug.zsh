4e306c79bb01110c7e8ef1221ff5291e8c3024fb
Sho Nakatani <lay.sakura@gmail.com>
2021-10-07 11:23:19 +0900

    function first

--- .zsh/001-zplug.zsh
+++ .zsh/001-zplug.zsh
@@ -1,4 +1,8 @@
-source /usr/local/opt/zplug/init.zsh || source /usr/share/zplug/init.zsh
+if is_osx; then
+  source /usr/local/opt/zplug/init.zsh
+elif is_linux; then
+  source /usr/share/zplug/init.zsh
+fi
 
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting"
