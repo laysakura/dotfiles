68dde6f409966b032488987bd63fe5fdb93eb44c
Sho Nakatani <lay.sakura@gmail.com>
2024-01-03 13:09:27 +0900

    use atuin for history

--- .zsh/100-peco.zsh
+++ .zsh/100-peco.zsh
@@ -1,16 +1,3 @@
-# ヒストリ補完
-function pecoHistorySelection() {
-    if has tac; then
-        tac='tac'
-    else
-        tac='tail -r'
-    fi
-    BUFFER=$(history -n 1 | eval $tac  | awk '!a[$0]++' | peco)
-    CURSOR=$#BUFFER
-    zle reset-prompt
-}
-zle -N pecoHistorySelection
-
 # リポジトリにcd
 function pecoCdRepo () {
     local selectedDir=$(ghq list -p | peco --query "$LBUFFER")
